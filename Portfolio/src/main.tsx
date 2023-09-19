2902ce3249ed1ed8bb43a8cf8394857adb386edf
Ethan <ethanhampton00@gmail.com>
2023-09-19 12:35:16 -0400

    Commit for computer update

--- Portfolio/src/main.tsx
+++ Portfolio/src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Portfolio from './Portfolio.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<ChakraProvider>
-			<Portfolio />
-		</ChakraProvider>
+		<Portfolio />
 	</React.StrictMode>,
 )
-
