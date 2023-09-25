791951123f56f80c45ca06ccee2370eb8e99acdd
Ethan Hampton <ethanhampton00@gmail.com>
2023-09-25 16:33:04 -0400

    changes grid sizes to rem and matches max width

--- Portfolio/src/components/sections/about-me.tsx
+++ Portfolio/src/components/sections/about-me.tsx
@@ -5,13 +5,10 @@ const About = () => {
 		<>
 			<div id='about' className='about-card section-card'>
 				<h2>About Me</h2>
-				<p>
+				<p className='about-me-content'>
 					I am a full stack developer with a passion for learning and a love for problem solving. I am currently working as a full stack developer at{' '}
 					<a href='https://www.neudesic.com/'>Neudesic</a> in Columbus, Ohio. I am currently working on my degree in Management Information Systems at{' '}
 					<a href='https://www.osu.edu/'>THE Ohio State University</a>.
-				</p>
-				<p>
-					<i>Link to page here</i>
 				</p>
 			</div>
 		</>
--- Portfolio/src/components/sections/experience.tsx
+++ Portfolio/src/components/sections/experience.tsx
@@ -3,10 +3,9 @@ import './sectionStyles.css'
 const Experience = () => {
 	return (
 		<>
-			<div id="experience" className='experience-card section-card'>
+			<div id='experience' className='experience-card section-card'>
 				<h2>Experience</h2>
-				<p>My experience is in React and JavaScript frontend, C# backend development, .NET, Java, and SQL.</p>
-				<p>I have a certification for the AZ-900</p>
+				<p className='.experience-content'>My experience is in React and JavaScript frontend, C# backend development, .NET, Java, and SQL. I have a certification for the AZ-900</p>
 			</div>
 		</>
 	)
